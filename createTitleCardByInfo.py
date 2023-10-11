b57fc06eb2d7a4606afa4fa5d746f8f6dbbbfebb
Jaewon Youn <78005200+larry1121@users.noreply.github.com>
2023-10-11 10:32:29 +0000

    이모지 깨짐 현상 해결

--- createTitleCardByInfo.py
+++ createTitleCardByInfo.py
@@ -1,25 +1,26 @@
 from PIL import Image, ImageDraw, ImageFont
 import os
+from remove_emoji import remove_emoji
+
+
 
 def createTitleCardByInfo(BlogMetaInfo):
 
-
-
-    post_title= BlogMetaInfo['title']
+    post_title = remove_emoji(str(BlogMetaInfo['title']))
     blog_name = BlogMetaInfo['site_name']
 
     # 이미지 크기
-    size = (500, 500)
+    size = (1080, 1080)  # Instagram card news size
     background_color = (255, 220, 220)  # 연한 핑크색
     font_path = "/workspaces/CardNewsAutomation/THE소녀감성.ttf"
-    min_font_size = 20
-    max_font_size = 60
+    min_font_size = 40
+    max_font_size = 120
     text_color = (0, 0, 0)
     border_color = (255, 255, 255)
     border_width = 5
 
     # 블로그 이름과 글의 제목
-    blog_font_size = 30
+    blog_font_size = 60
 
     # 이미지 생성
     image = Image.new("RGB", size, background_color)
@@ -51,15 +52,15 @@ def createTitleCardByInfo(BlogMetaInfo):
     # 테두리 그리기
     draw.rectangle((0, 0, size[0] - 1, size[1] - 1), outline=border_color, width=border_width)
 
-    # 이미지 저장
+    # 이미지 저장 (as JPG)
     if not os.path.exists("images"):
         os.makedirs("images")
-    filename = os.path.join("images", f"{post_title}.webp")  # Save as WebP format
-    image.save(filename, format="WEBP")
-    print(f"{post_title}.webp 완성")
+    filename = os.path.join("images", f"{post_title}.jpg")  # Save as JPG format
+    image.save(filename, format="JPEG")
+    print(f"{post_title}.jpg 완성")
+
 
 
 if __name__ == "__main__":
     # Test with different blog names and post titles
     createTitleCardByInfo({'site_name': 'giftedmbti', 'title': '[MBTI] INTJ는 왜 그럴까? 😎'})
-
---
+++ remove_emoji.py
@@ -0,0 +1,16 @@
+import emoji
+
+def remove_emoji(input_string):
+    # 이모지를 찾아내기
+    emojis = [c for c in input_string if emoji.is_emoji(c)]
+
+    # 이모지를 제거한 문자열 생성
+    string_without_emoji = ''.join([c for c in input_string if c not in emojis])
+
+    return string_without_emoji
+
+# 예제 사용
+original_string = "[MBTI] INTJ는 왜 그럴까? 😎"
+string_without_emoji = remove_emoji(original_string)
+print(f"Original String: {original_string}")
+print(f"String without Emoji: {string_without_emoji}")
